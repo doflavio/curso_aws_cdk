6301be73e28159d39dffe9bec168d53331534b10
Flávio Pereira <doflavio@gmail.com>
2021-08-01 17:02:37 -0300

    Configurando o autoscaling da

---
+++ src/main/java/com/myorg/DdbStack.java
@@ -0,0 +1,58 @@
+package com.myorg;
+
+import software.amazon.awscdk.core.*;
+import software.amazon.awscdk.services.dynamodb.*;
+
+public class DdbStack extends Stack {
+    private final Table productEventsDdb;
+
+    public DdbStack(final Construct scope, final String id) {
+        this(scope, id, null);
+    }
+
+    public DdbStack(final Construct scope, final String id, final StackProps props) {
+        super(scope, id, props);
+
+        productEventsDdb = Table.Builder.create(this, "ProductEventsDb")
+                .tableName("product-events")
+                .billingMode(BillingMode.PROVISIONED)
+                .readCapacity(1)
+                .writeCapacity(1)
+                .partitionKey(Attribute.builder()
+                        .name("pk")
+                        .type(AttributeType.STRING)
+                        .build())
+                .sortKey(Attribute.builder()
+                        .name("sk")
+                        .type(AttributeType.STRING)
+                        .build())
+                .timeToLiveAttribute("ttl")
+                .removalPolicy(RemovalPolicy.DESTROY)
+                .build();
+
+        productEventsDdb.autoScaleReadCapacity(EnableScalingProps.builder()
+                .minCapacity(1)
+                .maxCapacity(4)
+                .build())
+                .scaleOnUtilization(UtilizationScalingProps.builder()
+                        .targetUtilizationPercent(50)
+                        .scaleInCooldown(Duration.seconds(30))
+                        .scaleInCooldown(Duration.seconds(30))
+                        .build());
+
+        productEventsDdb.autoScaleWriteCapacity(EnableScalingProps.builder()
+                .minCapacity(1)
+                .maxCapacity(4)
+                .build())
+                .scaleOnUtilization(UtilizationScalingProps.builder()
+                        .targetUtilizationPercent(50)
+                        .scaleInCooldown(Duration.seconds(30))
+                        .scaleInCooldown(Duration.seconds(30))
+                        .build());
+    }
+
+    public Table getProductEventsDdb() {
+        return productEventsDdb;
+    }
+}
+
--- src/main/java/com/myorg/RdsStack.java
+++ src/main/java/com/myorg/RdsStack.java
@@ -1,17 +1,56 @@
 package com.myorg;
 
-import software.amazon.awscdk.core.Construct;
-import software.amazon.awscdk.core.Stack;
-import software.amazon.awscdk.core.StackProps;
+import software.amazon.awscdk.core.*;
+import software.amazon.awscdk.services.ec2.*;
+import software.amazon.awscdk.services.rds.*;
 
-public class CursoAwsCdkStack extends Stack {
-    public CursoAwsCdkStack(final Construct scope, final String id) {
-        this(scope, id, null);
+import java.util.Collections;
+
+public class RdsStack extends Stack {
+    public RdsStack(final Construct scope, final String id, Vpc vpc) {
+        this(scope, id, null, vpc);
     }
 
-    public CursoAwsCdkStack(final Construct scope, final String id, final StackProps props) {
+    public RdsStack(final Construct scope, final String id, final StackProps props, Vpc vpc) {
         super(scope, id, props);
 
-        // The code that defines your stack goes here
+        CfnParameter databasePassword = CfnParameter.Builder.create(this, "databasePassword")
+                .type("String")
+                .description("The RDS instance password")
+                .build();
+
+        ISecurityGroup iSecurityGroup = SecurityGroup.fromSecurityGroupId(this, id, vpc.getVpcDefaultSecurityGroup());
+        iSecurityGroup.addIngressRule(Peer.anyIpv4(), Port.tcp(3306));
+
+        DatabaseInstance databaseInstance = DatabaseInstance.Builder
+                .create(this, "Rds01")
+                .instanceIdentifier("aws-project01-db")
+                .engine(DatabaseInstanceEngine.mysql(MySqlInstanceEngineProps.builder()
+                        .version(MysqlEngineVersion.VER_5_6)
+                        .build()))
+                .vpc(vpc)
+                .credentials(Credentials.fromUsername("admin",
+                        CredentialsFromUsernameOptions.builder()
+                                .password(SecretValue.plainText(databasePassword.getValueAsString()))
+                                .build()))
+                .instanceType(InstanceType.of(InstanceClass.BURSTABLE2, InstanceSize.MICRO))
+                .multiAz(false)
+                .allocatedStorage(10)
+                .securityGroups(Collections.singletonList(iSecurityGroup))
+                .vpcSubnets(SubnetSelection.builder()
+                        .subnets(vpc.getPrivateSubnets())
+                        .build())
+                .build();
+
+        CfnOutput.Builder.create(this, "rds-endpoint")
+                .exportName("rds-endpoint")
+                .value(databaseInstance.getDbInstanceEndpointAddress())
+                .build();
+
+        CfnOutput.Builder.create(this, "rds-password")
+                .exportName("rds-password")
+                .value(databasePassword.getValueAsString())
+                .build();
+
     }
 }
