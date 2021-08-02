ccf172f9d7e7bb76cea81db284c5c841f93f011d
Flávio Leandro Pereira <doflavio@gmail.com>
2021-08-02 16:07:04 -0300

    Atribuindo a permissão à tarefa do ECS para acessar o SQS e o S3

--- src/main/java/com/myorg/CursoAwsCdkApp.java
+++ src/main/java/com/myorg/CursoAwsCdkApp.java
@@ -20,11 +20,16 @@ public class CursoAwsCdkApp {
 
         SnsStack snsStack = new SnsStack(app, "Sns");
 
+        InvoiceAppStack invoiceAppStack = new InvoiceAppStack(app, "InvoiceApp");
+
         Service01Stack service01Stack = new Service01Stack(app, "Service01",
-                clusterStack.getCluster(), snsStack.getProductEventsTopic());
+                clusterStack.getCluster(), snsStack.getProductEventsTopic(),
+                invoiceAppStack.getBucket(), invoiceAppStack.getS3InvoiceQueue());
+
         service01Stack.addDependency(clusterStack);
         service01Stack.addDependency(rdsStack);
         service01Stack.addDependency(snsStack);
+        service01Stack.addDependency(invoiceAppStack);
 
         DdbStack ddbStack = new DdbStack(app, "Ddb");
         Service02Stack service02Stack = new Service02Stack(app, "Service02",
